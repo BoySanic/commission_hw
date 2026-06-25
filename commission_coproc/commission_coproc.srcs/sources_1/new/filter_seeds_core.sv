fa91b90a6924bf6b3aaccb0ea369d2ddb1b3b27b
BoySanic <boysonicrevived@gmail.com>
2026-06-25 16:57:49 -0700

    Worked on the filter_seeds_core. Primarily just gluing the nextInteger stuff in

--- commission_coproc/commission_coproc.srcs/sources_1/new/filter_seeds_core.sv
+++ commission_coproc/commission_coproc.srcs/sources_1/new/filter_seeds_core.sv
@@ -26,15 +26,31 @@ module filter_seeds_core(
 );
     reg  [127:0] hash_octave [2:0];
     reg  [127:0] hash_continentalness;
-    reg  [127:0] seed_fork, noise_random;
-    reg  [127:0] noise_a_yo_fork, noise_b_yo_fork;
+    reg  [127:0] seed_fork, noise_a_hi, noise_b_hi;
+    reg  [127:0] noise_random[32:0];
+    reg  [127:0] noise_a_yo_fork[32:0], noise_b_yo_fork[32:0];
     reg  [127:0] noise_a_fork_stages[32:0], noise_a_fork_stages[32:0];
     reg  [127:0] c_0A_yo, c_0B_yo, c_1A_yo, c_1B_yo, c_2A_yo, c_2B_yo;
     
+    // 88 clocks each iirc 
     XrsrRandom_seed_fork seed_forker (
         .seed (seed_in),
         .clock (clock),
         .xoroshiro_state (seed_fork)
+    );
+    // 25 clocks each?
+    XrsrNextInternal noiserandom_fork_lo (
+	.xrsr_state_in (noise_random[0]),
+	.xrsr_state_out (noise_random[1]),
+	.clock (clock),
+	.next_long (noise_a_fork_stages[0][63:0])
+    );
+    // 25 clocks each?
+    XrsrNextInternal noiserandom_fork_hi (
+        .xrsr_state_in (noise_random[1]),
+	.xrsr_state_out (noise_random[2]),
+	.clock (clock),
+	.next_long (noise_a_yo_fork_stages[
     );
     initial begin
         hash_octave[0] = 128'h4e96119e22dedc81082fe255f8be6631;
@@ -45,10 +61,17 @@ module filter_seeds_core(
     
     always @(posedge clock) begin
         // seed_fork.from(hash_continentalness)
-        noise_random <= seed_fork ^ hash_continentalness;
+        noise_random[0] <= seed_fork ^ hash_continentalness;
     end
     
     always @(posedge clock) begin
-        noise_a_fork_stages[0] = 
+	// Skip 25 cycles
+	integer a_fork_index;
+	for (a_fork_index = 1; a_fork_index < 26; a_fork_index = a_fork_index + 1) begin
+       		noise_a_fork_stages[a_fork_index] <= noise_a_fork_stages[a_fork_index-1];
+	end
+	noise_a_fork_stages[26] <= {noise_a_hi, noise_a_fork_stages[25][63:0]};
+	noise_a_yo_fork_stages[0] <= noise_a_fork_stages[26];
+	noise_a_yo_fork_stages[1] // TODO: noise_yo_fork in a separate module or...?
     end
 endmodule
