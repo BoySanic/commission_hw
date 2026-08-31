c5c9f500deb8c408a05be1b06a5399a100f201ad
BoySanic <boysonicrevived@gmail.com>
2026-08-31 14:24:12 -0700

    Create add_u64 because I missed that before

---
+++ commission_coproc_asic/add_u64.sv
@@ -0,0 +1,19 @@
+module add_u32 (
+    input  [63:0] A,
+    input  [63:0] B,
+    output [63:0] S,
+    input  CLK,
+    input  CE
+);
+    reg [63:0] stages [6:0];
+
+always @(posedge CLK) begin
+    stages[0] <= A + B;
+    stages[1] <= stages[0];
+    stages[2] <= stages[1];
+    stages[3] <= stages[2];
+    stages[4] <= stages[3];
+    stages[5] <= stages[4];
+    S <= stages[5];
+end
+endmodule
