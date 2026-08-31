e55631ac097160ebce074728592c93c53c871086
BoySanic <boysonicrevived@gmail.com>
2026-08-31 14:28:24 -0700

    create mix2_multiplier

---
+++ commission_coproc_asic/mix2_multiplier.sv
@@ -0,0 +1,20 @@
+localparam XRSR_MIX2 = 64'h94d049bb133111eb;
+
+module mix2_multiplier (
+    input  [31:0] A,
+    output [31:0] P,
+    input  CLK,
+    input  CE
+);
+    reg [31:0] stages [6:0];
+
+always @(posedge CLK) begin
+    stages[0] <= A * XRSR_MIX2;
+    stages[1] <= stages[0];
+    stages[2] <= stages[1];
+    stages[3] <= stages[2];
+    stages[4] <= stages[3];
+    stages[5] <= stages[4];
+    P <= stages[5];
+end
+endmodule
